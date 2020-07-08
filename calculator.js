b892193f5de04468c462ce6725d7599ae5ae4c98
anthony-kyle-mcgrath <akm@anthonykyle.co.nz>
2020-07-08 16:50:14 +1200

    Calculator layout and styling

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// functions:
+//       AC = clear entry + clear memory
+//       CE = clear entry
+//       % = calculate percentage
+//       / = divide
+//       x = multiply
+//       - = subtract
+//       + = add
+//       = = process
+
+//       When you click on a button, add to array of numbers which are readout on screen
+//       click a modifier save entries as number to temporary placeholder
+//       add next lot of numbers to array
+//       on clicking any other button, perform arithmatic between entered and stored numbers. 
+//       display result, add to temp
