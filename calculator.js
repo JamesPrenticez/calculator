9e5b2ae9ee61af2ff2a204d7bce0ab7f1299f893
anthony-kyle-mcgrath <akm@anthonykyle.co.nz>
2020-07-09 10:35:58 +1200

    Process Numbers and operand entry

--- calculator.js
+++ calculator.js
@@ -18,13 +18,14 @@ document.addEventListener('DOMContentLoaded', addListeners);
 
 var entries = []; // Array for storing entered numbers
 var temp = 0;     // Temporary location for storing processed numbers
+var operand = "";
 
 function addListeners(){
   let keys = document.getElementsByClassName('key');
   for (let i = 0; i < keys.length; i++){
     keys[i].addEventListener('click', processClick);
   }
-  document.addEventListener('keypress', processKeypress);
+  document.addEventListener('keydown', processKeypress);
 } // addListeners()
 
 function processClick(evt){
@@ -37,15 +38,112 @@ function processKeypress(evt){
 
 function interpretEntry(key){
   console.log(key);
+  switch (key){
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      addEntry(parseInt(key));
+      break;
+    case ".":
+      if (entries.includes(".")){
+        break;
+      }
+      addEntry(key);
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      addOperand(key);
+      break;
+    case "%":
+      calcPercent();
+      break;
+    case "ac":
+    case "Escape":
+      allClear();
+      break;
+    case "ce":
+      clearEvent();
+      break;    
+    case "Enter":
+      calculate();
+      break;
+  };
 } // interpretEntry(key)
 
+function updateScreen(bool){
+  let screen = document.getElementById('numbers');
+
+  if (bool === true){
+    screen.textContent = entries.join("");
+  } else {
+    screen.textContent = "--MEM.ERR--";
+  }  
+} // updateScreen(bool)
+
 function addEntry(int){
-  entries.push(int);
+  if (entries.length < 12){
+    entries.push(int);
+    updateScreen(true);
+  } else {
+    updateScreen(false);
+  }
 } // addEntry(int)
 
-function updateScreen(){
-  document.getElementById('numbers').textContent = entries.join("");
-}
+function addOperand(key){
+  if (entries.length > 1){
+    temp = parseFloat(entries.join(""));
+    entries = [];
+  }  
+  operand = key;
+  switch(operand){
+    case "+":
+      toggleOperandDisplay("plus");
+      break;
+    case "-":
+      toggleOperandDisplay("minus");
+      break;
+    case "*":
+      toggleOperandDisplay("multiply");
+      break;
+    case "/":
+      toggleOperandDisplay("divide");
+      break;
+  };
+} // addOperand(key)
+
+function toggleOperandDisplay(operand){
+  let opClass = document.getElementsByClassName("operand");
+  for (let i = 0; i < opClass.length; i++){
+    if (opClass[i].id == operand){
+      opClass[i].style.display = "block";
+    } else {
+      opClass[i].style.display = "none";
+    }
+    
+  }
+
+} // toggleOperandDisplay(operand)
+
+function calcPercent() {
+
+} // calcPercent()
+
+function allClear(){
+
+} // allClear()
+
+function clearEvent(){
+
+} // clearEvent()
 
 function calculate(){
 
